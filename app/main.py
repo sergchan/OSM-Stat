9020058b44c589afd98d7066126d34801285ef90
sergchan <41125007+sergchan@users.noreply.github.com>
2026-07-03 13:07:39 +0300

    test folium map

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.responces import HTMLResponce
+from fastapi.responses import HTMLResponse
 from utils import json_to_dict_list
 import os
 from typing import Optional
